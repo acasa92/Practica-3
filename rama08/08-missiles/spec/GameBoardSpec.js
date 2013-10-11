100603192190d23fea3a96c43a51a09f7c3a6b7f
Alejandro Campos Sanchez <alexbetis92@gmail.com>
2013-10-11 19:15:44 +0200

    on no funcionables en 08

--- rama08/08-missiles/spec/GameBoardSpec.js
+++ rama08/08-missiles/spec/GameBoardSpec.js
@@ -1,42 +1,186 @@
 /*
 
-
-En el anterior prototipo, el objeto Game permite gestionar una pila de
-tableros (boards). Los tres campos de estrellas, la pantalla de inicio
-y el sprite de la nave del jugador se añaden como tableros
-independientes para que Game pueda ejecutar sus métodos step() y
-draw() periódicamente desde su método loop(). Sin embargo los tableros
-no pueden interaccionar entre sí. Resulta difícil con esta
-arquitectura pensar en cómo podría por ejemplo detectarse la colisión
-de una nave enemiga con la nave del jugador, o cómo podría detectarse
-si un disparo de colisiona con una nave.
-
-Este es precisamente el requisito que se ha identificado para este
-prototipo: gestionar la interacción entre los elementos del
-juego. Piensa en esta clase como un tablero de juegos de mesa, sobre
-el que se disponen los elementos del juego (fichas, cartas, etc.). En
-este caso serán naves enemigas, nave del jugador y disparos los
-elementos del juego. Para Game, GameBoard será un tablero más, por lo
-que deberá ofrecer los métodos step() y draw(), y será responsable de
-mostrar todos los objetos que contenga cuando Game llame a estos
-métodos.
+  En el anterior prototipo (06-player), el objeto Game permite
+  gestionar una colección de tableros (boards). Los tres campos de
+  estrellas, la pantalla de inicio, y el sprite de la nave del
+  jugador, se añaden como tableros independientes para que Game pueda
+  ejecutar sus métodos step() y draw() periódicamente desde su método
+  loop(). Sin embargo los objetos que muestran los tableros no pueden
+  interaccionar entre sí. Aunque se añadiesen nuevos tableros para los
+  misiles y para los enemigos, resulta difícil con esta arquitectura
+  pensar en cómo podría por ejemplo detectarse la colisión de una nave
+  enemiga con la nave del jugador, o cómo podría detectarse si un
+  misil disparado por la nave del usuario ha colisionado con una nave
+  enemiga.
 
 
+  Requisitos:
 
-Especificación: GameBoard debe
+  Este es precisamente el requisito que se ha identificado para este
+  prototipo: diseñar e implementar un mecanismo que permita gestionar
+  la interacción entre los elementos del juego. Para ello se diseñará
+  la clase GameBoard. Piensa en esta clase como un tablero de un juego
+  de mesa, sobre el que se disponen los elementos del juego (fichas,
+  cartas, etc.). En Alien Invasion los elementos del juego serán las
+  naves enemigas, la nave del jugador y los misiles. Para el objeto
+  Game, GameBoard será un board más, por lo que deberá ofrecer los
+  métodos step() y draw(), siendo responsable de mostrar todos los
+  objetos que contenga cuando Game llame a estos métodos.
 
-- mantener una colección de objetos a la que se pueden añadir y de la
-  que se pueden eliminar sprites
+  Este prototipo no añade funcionalidad nueva a la que ofrecía el
+  prototipo 06.
 
-- interacción con Game: cuando reciba los métodos step() y draw() debe
-  ocuparse de que se ejecuten estos métodos en todos los objetos que
-  contenga.
 
-- debe detectar la colisión entre objetos. Querremos que los disparos
-  de la nave del jugador detecten cuándo colisionan con una nave
-  enemiga, que una nave enemiga detecte si colisiona con la nave del
-  jugador, que un disparo de la nave enemiga detecte si colisiona con
-  la nave del jugador,... necesitamos saber de qué tipo es cada objeto.
+  Especificación: GameBoard debe
 
+  - mantener una colección a la que se pueden añadir y de la que se
+    pueden eliminar sprites como nave enemiga, misil, nave del
+    jugador, explosión, etc.
+
+  - interacción con Game: cuando Game llame a los métodos step() y
+    draw() de un GameBoard que haya sido añadido como un board a Game,
+    GameBoard debe ocuparse de que se ejecuten los métodos step() y
+    draw() de todos los objetos que contenga
+
+  - debe ofrecer la posibilidad de detectar la colisión entre
+    objetos. Un objeto sprite almacenado en GameBoard debe poder
+    detectar si ha colisionado con otro objeto del mismo
+    GameBoard. Los misiles disparados por la nave del jugador deberán
+    poder detectar gracias a esta funcionalidad ofrecida por GameBoard
+    cuándo han colisionado con una nave enemiga; una nave enemiga debe
+    poder detectar si ha colisionado con la nave del jugador; un misil
+    disparado por la nave enemiga debe poder detectar si ha
+    colisionado con la nave del jugador. Para ello es necesario que se
+    pueda identificar de qué tipo es cada objeto sprite almacenado en
+    el tablero de juegos, pues cada objeto sólo quiere comprobar si ha
+    colisionado con objetos de cierto tipo, no con todos los objetos.
 
 */
+describe("Clase GameBoard", function(){
+
+    var canvas, ctx;
+
+    beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+
+    });
+    
+    
+    it("add", function(){
+	
+	var miboard = new GameBoard();
+	var obj1=0;
+	var obj2=5;
+	miboard.add(obj1);
+	miboard.add(obj2);
+	expect(miboard.objects[0]).toEqual(0);
+	expect(miboard.objects[1]).toEqual(5);
+	});
+    it("remove&delete", function(){
+	
+	var miboard = new GameBoard();
+	var obj1=0;
+	var obj2=5;
+	miboard.add(obj1);
+	miboard.add(obj2);
+	miboard.resetRemoved();
+	miboard.remove(obj1);
+	miboard.remove(obj2);
+	expect(miboard.removed[0]).toEqual(0);
+	expect(miboard.removed[1]).toEqual(5);
+	miboard.finalizeRemoved();
+	expect(miboard.objects[0]).toEqual(undefined);
+	
+	});
+    it("overlap", function(){
+	
+	var miboard = new GameBoard();
+	var obj1={
+	    x:2,
+	    y:4,
+	    w:8,
+	    h:9
+	};
+	var obj2={
+	    x:2,
+	    y:4,
+	    w:8,
+	    h:9
+	};
+	miboard.add(obj1);
+	miboard.add(obj2);
+	
+ 	expect(miboard.overlap(obj1,obj2)).toEqual(true);
+  	
+	});
+    it("draw", function(){
+	
+
+	var miboard = new GameBoard();
+	spyOn(miboard, "draw");
+	var obj1={
+	    x:2,
+	    y:4,
+	    w:8,
+	    h:9
+	};
+	miboard.add(obj1);
+	miboard.draw(ctx);
+	waits(200);
+	runs(function(){
+ 		expect(miboard.draw).toHaveBeenCalled();
+	});
+
+    });
+
+    it("step", function(){
+	
+
+	var miboard = new GameBoard();
+	spyOn(miboard, "step");
+	var obj1={
+	    x:2,
+	    y:4,
+	    w:8,
+	    h:9
+	};
+	miboard.add(obj1);
+	miboard.step(ctx);
+	waits(200);
+	runs(function(){
+ 		expect(miboard.step).toHaveBeenCalled();
+	});
+
+    });
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
--- rama08/08-missiles/spec/PlayerMissileSpec.js
+++ rama08/08-missiles/spec/PlayerMissileSpec.js
@@ -27,3 +27,79 @@
     la clase en el prototipo
 
 */
+
+describe("Clase PlayerMissile", function(){
+    // Una vez comenzado el juego deberá aparecer la nave del jugador en
+    // la parte inferior
+
+    // La nave debera moverse a izquierda y derecha con las teclas de las
+    // flechas izda y dcha
+
+    var canvas, ctx;
+
+    beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+
+    });
+
+
+
+    it("draw", function(){
+	// Comprobamos que draw llama a SpriteSheet.draw con los
+	// parametros adecuados
+	var misil = new PlayerMissile(1,2);
+	spyOn(SpriteSheet, "draw");
+
+	// Necesitamos tener Game.width y Game.height para que el
+	// constructor de PlayerShip pueda inicializar x e y
+	Game = {width: 320, height: 480};
+
+
+
+	misil.draw(ctx);
+	waits(200);
+	runs(function(){
+		expect(misil.draw).toHaveBeenCalled();
+ 		expect(misil.draw.calls[0].args[1]).toEqual('missile');
+ 		expect(misil.draw.calls[0].args[2]).toEqual(1);
+ 		expect(misil.draw.calls[0].args[3]).toEqual(2);
+ 		expect(misil.draw.calls[0].args[4]).toEqual(0);
+	});
+	
+    });
+
+
+    /*it("step sin teclas pulsadas", function(){
+	// Escribe tests para la clase PlayerShip
+	// Deberian comprobar que:
+
+	// 2. Tras llamar a step sin haber pulsado teclas la nave se
+	// muestra en el mismo lugar
+
+	// 3. Tras llamar a step con una flecha pulsada, se actualiza
+	// la posicion de la nave
+
+
+	// Necesitamos tener Game.width y Game.height para que el
+	// constructor de PlayerShip pueda inicializar x e y.  Y
+	// necesitamos Geame.keys para saber si se ha pulsado una
+	// tecla
+	Game = {width: 320, height: 480, keys: {'left': false}};
+	
+	// Creamos un PlayerShip para testar
+	var miNave = new PlayerShip();	
+
+ 	miNave.step(1); // Hacemos como que ha pasado 1 segundo
+	// Tras step, con Game.keys['left'] == false, no debe haberse movido,
+        // por lo que lo comparamos con la posición x inicial de PlayerShip
+	expect(miNave.x).toEqual(Game.width/2 - miNave.w / 2);
+
+
+    });*/
+});
